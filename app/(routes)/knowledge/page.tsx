89a737307bd45873a9b6928790030585d7b508f4
samKenpachi011 <samuelkabelo1@gmail.com>
2025-03-01 22:38:45 +0200

    ADD: base nav

---
+++ app/(routes)/knowledge/page.tsx
@@ -0,0 +1,15 @@
+import { PageHeader } from "@/app/components/shared/page-header";
+
+export default function KnowledgePage() {
+    return (
+      <div className="container p-12 md:p-12">
+        <PageHeader
+          title="Indigenous Knowledge"
+          description="Explore Botswana's rich heritage of traditional knowledge, from medicinal plants to cultural expressions."
+        />
+
+
+
+      </div>
+    );
+  }
